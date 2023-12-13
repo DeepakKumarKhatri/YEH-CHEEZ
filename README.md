3623b03ef8a5b33569c035b394746615802950a0
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2023-12-13 23:45:28 +0500

    Added required dependencies to get started

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # YEH-CHEEZ
 A Marketplace for Consumers and Sellers
+
+
+Figma Link: https://www.figma.com/file/CujCzD8Y7gReqVGinkBqs1/MAD-project?type=design&mode=design
---
+++ Yeh_Cheez/App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {createDrawerNavigator} from '@react-navigation/drawer'
+
+// const drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
